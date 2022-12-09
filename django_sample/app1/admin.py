6cfdc7f3782ca19384feb2936d99ad2d31fdcf60
ARFCAC <xingsantianzhong460@gmail.com>
2022-12-10 00:36:36 +0900

    v1.0

--- django_sample/app1/admin.py
+++ django_sample/app1/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import TestData
+# from .models import TestData
 
 
-@admin.register(TestData)
-class ProductDataAdmin(admin.ModelAdmin):
-    model = TestData
-    list_display = ["number", "name", "price"]
-    ordering = ["number"]
+# @admin.register(TestData)
+# class ProductDataAdmin(admin.ModelAdmin):
+#     model = TestData
+#     list_display = ["number", "name", "price"]
+#     ordering = ["number"]
--- django_sample/app1/models.py
+++ django_sample/app1/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 
-class TestData(models.Model):
-    number = models.PositiveIntegerField()
-    name = models.CharField(max_length=200, blank=False, null=False)
-    price = models.PositiveIntegerField()
+# class TestData(models.Model):
+#     number = models.PositiveIntegerField()
+#     name = models.CharField(max_length=200, blank=False, null=False)
+#     price = models.PositiveIntegerField()
 
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
 
-    class Meta:
-        verbose_name_plural = "テストデータ"
+#     class Meta:
+#         verbose_name_plural = "テストデータ"
